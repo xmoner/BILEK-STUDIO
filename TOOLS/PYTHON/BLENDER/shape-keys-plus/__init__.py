a211ee70feb5a98465b6da0a650633a33af7def7
xmoner <lucas.bilek@gmail.com>
2021-06-12 20:11:11 +0100

    Added GPL License and updated zip files.

--- TOOLS/PYTHON/BLENDER/shape-keys-plus/__init__.py
+++ TOOLS/PYTHON/BLENDER/shape-keys-plus/__init__.py
@@ -1,3 +1,21 @@
+'''
+Shape Keys Plus add-on for Duplicating and Mirroring Shape Keys.
+Copyright (C) 2021  Lukas Bilek. BILEK STUDIO.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 from . import shape_keys_plus_main
 # Info about the add-on
 bl_info = {
